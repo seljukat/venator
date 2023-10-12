c190b3086e486ebfc73a0620aa434ec24e0a440e
Selçuk Topkaya <seljukat@gmail.com>
2023-10-12 17:27:29 +0300

    download functionality for the documents in Documents Page added

--- src/pages/DocumentationsPage.js
+++ src/pages/DocumentationsPage.js
@@ -1,61 +1,55 @@
 import '../styles/doc-cards.css';
 import { GrDocumentPdf } from 'react-icons/gr';
+import projectProposal from '../Documents/Project Proposal.pdf';
   
 const DocumentationsPage = () => (
     <>
-    
+   
     <div className="doc-cards docs-page-anim">
 
-
-        <a className='doc-link' href='#'>
+        <a className='doc-link' href={projectProposal} download="Project Proposal.pdf" target='_blank' rel='noreferrer'>
             <div className="doc-card">
                 <div className="doc-card-top">
                     <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
                 </div>
                 <div className="doc-card-container">
-                    <h4><b>SRS Document</b></h4> 
-                    <p>Project Initialization &amp; bla bla bla</p> 
+                    <h4><b>Project Proposal</b></h4> 
                 </div>
             </div>
         </a>
 
-        <a className='doc-link' href='#'>
+        {/* <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
             <div className="doc-card">
                 <div className="doc-card-top">
                     <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
                 </div>
                 <div className="doc-card-container">
                     <h4><b>Design Document</b></h4> 
-                    <p>Design bla bla bla</p> 
                 </div>
             </div>
         </a>
 
-        <a className='doc-link' href='#'>
+        <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
             <div className="doc-card">
                 <div className="doc-card-top">
                     <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
                 </div>
                 <div className="doc-card-container">
                     <h4><b>Other Document</b></h4> 
-                    <p>bla bla bla</p> 
                 </div>
             </div>
         </a>
 
-        <a className='doc-link' href='#'>
+        <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
             <div className="doc-card">
                 <div className="doc-card-top">
                     <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
                 </div>
                 <div className="doc-card-container">
                     <h4><b>The Other Document</b></h4> 
-                    <p>bla bla bla</p> 
                 </div>
             </div>
-        </a>
-
-
+        </a> */}
 
     </div>
 
