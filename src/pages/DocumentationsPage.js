4137a52e239fe918719f311aa8783d1401289949
Selçuk Topkaya <seljukat@gmail.com>
2023-10-28 18:02:33 +0300

    Project Specifications Report added into the Documents folder

--- src/pages/DocumentationsPage.js
+++ src/pages/DocumentationsPage.js
@@ -1,6 +1,7 @@
 import '../styles/doc-cards.css';
 import { GrDocumentPdf } from 'react-icons/gr';
 import projectProposal from '../Documents/Project Proposal.pdf';
+import projectSpecificationsReport from '../Documents/Project Specifications Report.pdf';
   
 const DocumentationsPage = () => (
     <>
@@ -18,40 +19,20 @@ const DocumentationsPage = () => (
             </div>
         </a>
 
-        {/* <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
+        <a className='doc-link' href={projectSpecificationsReport} target='_blank' rel='noreferrer'>
             <div className="doc-card">
                 <div className="doc-card-top">
                     <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
                 </div>
                 <div className="doc-card-container">
-                    <h4><b>Design Document</b></h4> 
+                    <h4><b>Project Specifications Report</b></h4> 
                 </div>
             </div>
         </a>
-
-        <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
-            <div className="doc-card">
-                <div className="doc-card-top">
-                    <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="doc-card-container">
-                    <h4><b>Other Document</b></h4> 
-                </div>
-            </div>
-        </a>
-
-        <a className='doc-link empty' href='#' target='_blank' rel='noreferrer'>
-            <div className="doc-card">
-                <div className="doc-card-top">
-                    <GrDocumentPdf style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="doc-card-container">
-                    <h4><b>The Other Document</b></h4> 
-                </div>
-            </div>
-        </a> */}
 
     </div>
+
+    {/* <iframe className='' src={projectProposal} height="100%" width="100%" title="Project Proposal"></iframe> */}
 
     </>
 )
