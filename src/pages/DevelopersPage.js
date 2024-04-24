f2c32f5f6a84960af34ef1e2a1ec32a975a7d845
Selçuk Topkaya <seljukat@gmail.com>
2024-04-24 21:13:01 +0300

    temporary update v2

--- src/pages/DevelopersPage.js
+++ src/pages/DevelopersPage.js
@@ -1,87 +1,155 @@
-import '../styles/flip-cards.css';
-import { RxAvatar } from 'react-icons/rx';
-import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineInstagram } from 'react-icons/ai';
+import "../styles/flip-cards.css";
+import { RxAvatar } from "react-icons/rx";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
 
 const DevelopersPage = () => (
-    <>
-    
+  <>
     <div className="flip-cards developers-page-anim">
-
-        <div className="flip-card">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <RxAvatar style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="flip-card-back">
-                    <h1 className='flip-name'>Arda</h1>
-                    <h1 className='flip-surname'>Uyaroğlu</h1> 
-                    <p>Computer Engineer</p> 
-                    <div className='flip-card-back-footer'>
-                        <div className='devs-icon-container'><a className='icon-link' href='#' target='_self' rel='noreferrer'><AiOutlineGithub className='flip-icon' /></a></div>
-                        <div className='between'></div>
-                        <div className='devs-icon-container'><a className='icon-link' href='https://www.linkedin.com/in/arda-uyaro%C4%9Flu/' target='_blank' rel='noreferrer'><AiOutlineLinkedin className='flip-icon' /></a></div>
-                    </div>
-                </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <RxAvatar style={{ width: "200px", height: "200px" }} />
+          </div>
+          <div className="flip-card-back">
+            <h1 className="flip-name">Arda</h1>
+            <h1 className="flip-surname">Uyaroğlu</h1>
+            <p>Computer Engineer</p>
+            <div className="flip-card-back-footer">
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://github.com/arda-uyaroglu"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineGithub className="flip-icon" />
+                </a>
+              </div>
+              <div className="between"></div>
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://www.linkedin.com/in/arda-uyaro%C4%9Flu/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineLinkedin className="flip-icon" />
+                </a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className="flip-card">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <RxAvatar style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="flip-card-back">
-                    <h1 className='flip-name'>Berker</h1>
-                    <h1 className='flip-surname'>Tomaç</h1> 
-                    <p>Computer Engineer</p> 
-                    <div className='flip-card-back-footer'>
-                        <div className='devs-icon-container'><a className='icon-link' href='#' target='_self' rel='noreferrer'><AiOutlineGithub className='flip-icon' /></a></div>
-                        <div className='between'></div>
-                        <div className='devs-icon-container'><a className='icon-link' href='https://www.linkedin.com/in/berkertomac/' target='_blank' rel='noreferrer'><AiOutlineLinkedin className='flip-icon' /></a></div>
-                    </div>
-                </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <RxAvatar style={{ width: "200px", height: "200px" }} />
+          </div>
+          <div className="flip-card-back">
+            <h1 className="flip-name">Berker</h1>
+            <h1 className="flip-surname">Tomaç</h1>
+            <p>Computer Engineer</p>
+            <div className="flip-card-back-footer">
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://github.com/BerkerTomac"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineGithub className="flip-icon" />
+                </a>
+              </div>
+              <div className="between"></div>
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://www.linkedin.com/in/berkertomac/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineLinkedin className="flip-icon" />
+                </a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className="flip-card">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <RxAvatar style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="flip-card-back">
-                    <h1 className='flip-name'>Selçuk</h1> 
-                    <h1 className='flip-surname'>Topkaya</h1>
-                    <p>Computer Engineer</p> 
-                    <div className='flip-card-back-footer'>
-                        <div className='devs-icon-container'><a className='icon-link' href='#' target='_self' rel='noreferrer'><AiOutlineGithub className='flip-icon' /></a></div>
-                        <div className='between'></div>
-                        <div className='devs-icon-container'><a className='icon-link' href='https://www.linkedin.com/in/sel%C3%A7uk-topkaya-233b02230/' target='_blank' rel='noreferrer'><AiOutlineLinkedin className='flip-icon' /></a></div>
-                    </div>
-                </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <RxAvatar style={{ width: "200px", height: "200px" }} />
+          </div>
+          <div className="flip-card-back">
+            <h1 className="flip-name">Selçuk</h1>
+            <h1 className="flip-surname">Topkaya</h1>
+            <p>Computer Engineer</p>
+            <div className="flip-card-back-footer">
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://github.com/seljukat"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineGithub className="flip-icon" />
+                </a>
+              </div>
+              <div className="between"></div>
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://www.linkedin.com/in/sel%C3%A7uk-topkaya-233b02230/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineLinkedin className="flip-icon" />
+                </a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className="flip-card">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <RxAvatar style={{width:"200px",height:"200px"}}/>
-                </div>
-                <div className="flip-card-back">
-                    <h1 className='flip-name'>Utku</h1>
-                    <h1 className='flip-surname'>Oktay</h1>
-                    <p>Computer Engineer</p> 
-                    <div className='flip-card-back-footer'>
-                        <div className='devs-icon-container'><a className='icon-link' href='#' target='_self' rel='noreferrer'><AiOutlineGithub className='flip-icon' /></a></div>
-                        <div className='between'></div>
-                        <div className='devs-icon-container'><a className='icon-link' href='https://www.linkedin.com/in/utku-oktay-9b30581b4/' target='_blank' rel='noreferrer'><AiOutlineLinkedin className='flip-icon' /></a></div>
-                    </div>
-                </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <RxAvatar style={{ width: "200px", height: "200px" }} />
+          </div>
+          <div className="flip-card-back">
+            <h1 className="flip-name">Utku</h1>
+            <h1 className="flip-surname">Oktay</h1>
+            <p>Computer Engineer</p>
+            <div className="flip-card-back-footer">
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://github.com/UtkuOktay"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineGithub className="flip-icon" />
+                </a>
+              </div>
+              <div className="between"></div>
+              <div className="devs-icon-container">
+                <a
+                  className="icon-link"
+                  href="https://www.linkedin.com/in/utku-oktay-9b30581b4/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineLinkedin className="flip-icon" />
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-
+      </div>
     </div>
-
-    </>
-)
+  </>
+);
 
 export default DevelopersPage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,8 +6,26 @@ const HomePage = () => (
     {/* <h1 className="home-page-content">Home Page</h1> */}
     <div className="flex">
       <div className="content">
-        <h1>Venator</h1>
-        <p>Statistics for your games.</p>
+        <h1>Discover the Game-Changer in Football Analysis: Venator!</h1>
+        <p className="text">
+          Elevate your team's <span className="underline">performance</span>{" "}
+          with Venator's cutting-edge technology. Harnessing the power of{" "}
+          <span className="underline">computer vision</span> and advanced{" "}
+          <span className="underline">machine learning</span>, Venator
+          meticulously{" "}
+          <span className="underline">analyzes football matches</span> to track
+          players, goalkeepers, referees, and the ball. Dive into detailed
+          statistics—player positions, distance covered, heat maps, and more.
+          From distinguishing team players to calculating ball possession,
+          Venator{" "}
+          <span className="underline">provides invaluable insights</span> that{" "}
+          <span className="underline">help coaches</span> make strategic
+          decisions. Get ready to{" "}
+          <span className="bold">
+            transform your game strategy and outplay the competition with
+            Venator!
+          </span>
+        </p>
       </div>
       <Slider />
     </div>
